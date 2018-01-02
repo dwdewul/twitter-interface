f6424513a9feb41cf99feed7a19e7c63bfb7005f
David DeWulf <dwdewul@gmail.com>
2018-01-01 19:13:02 -0600

    Built endpoints to grab data from Twitter API

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
+const Twitter = require('twitter');
 const keys = require('./config');
+
+const client = new Twitter({
+    consumer_key: keys.consumerKey,
+    consumer_secret: keys.consumerSecret,
+    access_token_key: keys.accessToken,
+    access_token_secret: keys.accessTokenSecret,
+});
+
+const queryParams = {
+    count: 5,
+    screen_name: 'dwdewul'
+}
+
+client.get('statuses/user_timeline', queryParams, (error, tweets, res) => {
+    if (error) {
+        console.error(error);
+        throw error;
+    }
+    tweets.map((val) => {
+        console.log(val.text);
+    });
+});
+
+client.get('friends/list', queryParams, (error, friends, res) => {
+    if (error) {
+        console.error(error);
+        throw error;
+    }
+    friends.users.map((val) => {
+        console.log(val.name);
+    });
+});
+
+client.get('direct_messages', queryParams, (error, messages, res) => {
+    if (error) {
+        console.error(error);
+        throw error;
+    }
+    messages.map((val) => {
+        console.log(val.text);
+    });
+});
