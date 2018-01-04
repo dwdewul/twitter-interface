b9e2b8bf25ee73d8b4a000fceb1ed1634f892a4d
David DeWulf <dwdewul@gmail.com>
2018-01-04 13:39:51 -0600

    Made major additions to the project refactored routes into a routes directory and implemented Express's router module refactored Twitter API call promises to only return the requisite info in the promise Built out the PUG templates and added the appropriate CSS classes

--- app.js
+++ app.js
@@ -2,60 +2,21 @@ const express = require('express');
 const pug = require('pug');
 const path = require('path');
 const bodyParser = require('body-parser');
-const Twitter = require('twitter');
-const keys = require('./config');
+const index = require('./routes');
+
 
 const app = express();
-app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/public', express.static(path.join(__dirname, 'public')));
+// set views folder
 app.set('views', './views');
+// Set up template engine with pug
 app.set('view engine', 'pug');
+// Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// set up the twitter client object
-const client = new Twitter({
-    consumer_key: keys.consumerKey,
-    consumer_secret: keys.consumerSecret,
-    access_token_key: keys.accessToken,
-    access_token_secret: keys.accessTokenSecret,
-});
+app.use('/', index);
 
-// query params for every API call
-const queryParams = {
-    count: 5,
-    screen_name: 'dwdewul'
-}
-
-const getTweets = () => {
-    return client.get('statuses/user_timeline', queryParams)
-        .then((tweets) => {
-            return tweets.map(val => val.text);
-        })
-        .catch(err => console.error(err));
-};
-
-const getFriends = () => {
-    return client.get('friends/list', queryParams)
-        .then((friends) => {
-            return friends.users.map(val => val.name);
-        })
-        .catch(err => console.error(err));
-};
-
-const getMessages = () => {
-    return client.get('direct_messages', queryParams)
-        .then((messages) => {
-            return messages.map(val => val.text);
-        })
-        .catch(err => console.error(err));
-};
-
-app.get('/', (req, res) => {
-    Promise.all([getTweets(), getFriends(), getMessages()])
-    .then(data => {
-        res.render(path.join(__dirname, 'views', 'index'), { data });
-    })
-    .catch(err => console.log('Promise.all error: ', err));
-});
 
 
 const PORT = process.env.PORT || 3000;
---
+++ routes/index.js
@@ -0,0 +1,76 @@
+const router = require('express').Router();
+const Twitter = require('twitter');
+const path = require('path');
+const moment = require('moment');
+const keys = require('../config');
+
+const client = new Twitter({
+    consumer_key: keys.consumerKey,
+    consumer_secret: keys.consumerSecret,
+    access_token_key: keys.accessToken,
+    access_token_secret: keys.accessTokenSecret,
+});
+
+// query params for every API call
+const queryParams = {
+    count: 5,
+    screen_name: 'dwdewul'
+}
+
+const getTweets = () => {
+    return client.get('statuses/user_timeline', queryParams)
+        .then((tweets) => {
+            let myTweets = [];
+            tweets.map(val => {
+                myTweets.push({
+                    time: moment(val.created_at).fromNow(),
+                    tweet: val.text,
+                    name: val.user.name,
+                    screenName: val.user.screen_name,
+                    reTweetCount: val.retweet_count,
+                    favoriteCount: val.favorite_count,
+                    profilePic: val.user.profile_image_url,
+                    friendCount: val.user.friends_count
+                })
+            });
+            return myTweets;
+        })
+        .catch(err => console.error(err));
+};
+
+const getFriends = () => {
+    return client.get('friends/list', queryParams)
+        .then((friends) => {
+            return friends.users;
+        })
+        .catch(err => console.error(err));
+};
+
+const getMessages = () => {
+    return client.get('direct_messages', queryParams)
+        .then((messages) => {
+            let directMessages = [];
+            messages.map(val => {
+                directMessages.push({
+                    messageTime: moment(val.created_at).fromNow(),
+                    messageText: val.text,
+                    senderProfilePic: val.sender.profile_image_url,
+                    senderScreenName: val.sender.screen_name,
+                    recipientProfilePic: val.recipient.profile_image_url,
+                    recipientMessageTime: moment(val.recipient.created_at).format('YYYY MM Do, h:mm:ss a')
+                });
+            });
+            return directMessages;
+        })
+        .catch(err => console.error(err));
+};
+
+router.get('/', (req, res) => {
+    Promise.all([getTweets(), getFriends(), getMessages()])
+    .then(data => {
+        res.render(path.join('../views', 'index'), { data, moment });
+    })
+    .catch(err => console.log('Promise.all error: ', err));
+});
+
+module.exports = router;
